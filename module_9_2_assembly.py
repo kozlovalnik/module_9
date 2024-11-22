5ff5bbe74e8cc8f5ad8c14900bc55d70a9fdb46f
Александр <kozlov.alexander.63@gmail.com>
2024-11-22 08:56:51 +0300

    Add files via upload

---
+++ module_9_2_assembly.py
@@ -0,0 +1,11 @@
+first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
+second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
+
+first_result = [len(string) for string in first_strings if len(string) >= 5]
+second_result = [(string1, string2) for string1 in first_strings for string2 in second_strings if
+                 len(string1) == len(string2)]
+third_result = {string: len(string) for string in first_strings + second_strings if not len(string) % 2}
+
+print(first_result)
+print(second_result)
+print(third_result)
